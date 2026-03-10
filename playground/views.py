7e6b91a177ab72060ca4d3cc0153155175ae7448
Sobur Ahmed <soburwebdevs@gmail.com>
2026-03-11 02:26:44 +0600

    create and execute celery tasks

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,9 @@
-from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
-from templated_mail.mail import BaseEmailMessage
+from .tasks import notify_customers
 
 
 def say_hello(request):
-    try:
-        message = BaseEmailMessage(
-            template_name='emails/hello.html',
-            context= {'name': 'Mosh'}
-        )
-        message.send(['john@moshbuy.com'])
-    except BadHeaderError:
-        pass
+    notify_customers.delay('Hello')
     return render(request, 'hello.html', {'name': 'Mosh'})
 
 
